11d68a2638e3af7c6d796d11c1b648c1749db90d
Nimunozb <nimunozb@unal.edu.co>
2023-06-05 13:08:53 -0500

    pantalla

---
+++ i5ether/test/EthNativePosix/backup/software/libliteeth/tftp.d
@@ -0,0 +1,52 @@
+tftp.o: \
+ /home/nimunozb/Documentos/embebidos/programs/litex/litex/litex/soc/software/libliteeth/tftp.c \
+ /home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/tinystdio/stdio.h \
+ /home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/inttypes.h \
+ /home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/newlib.h \
+ /home/nimunozb/Documentos/embebidos/repositorios/evse-embebidos-software/i5ether/test/EthNativePosix/build/software/include/../libc/picolibc.h \
+ /home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/config.h \
+ /home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/ieeefp.h \
+ /home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/features.h \
+ /root/miniconda3/envs/fpga/lib/gcc/riscv32-elf/10.1.0/include/float.h \
+ /home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_intsup.h \
+ /home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/_ansi.h \
+ /home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/stdint.h \
+ /home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_default_types.h \
+ /home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_stdint.h \
+ /root/miniconda3/envs/fpga/lib/gcc/riscv32-elf/10.1.0/include/stddef.h \
+ /home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_locale.h \
+ /root/miniconda3/envs/fpga/lib/gcc/riscv32-elf/10.1.0/include/stdarg.h \
+ /home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_types.h \
+ /home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_types.h \
+ /home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/string.h \
+ /home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/cdefs.h \
+ /home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/strings.h \
+ /home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/string.h \
+ /home/nimunozb/Documentos/embebidos/programs/litex/litex/litex/soc/software/libbase/progress.h \
+ /home/nimunozb/Documentos/embebidos/programs/litex/litex/litex/soc/software/libliteeth/udp.h \
+ /home/nimunozb/Documentos/embebidos/programs/litex/litex/litex/soc/software/libliteeth/tftp.h
+/home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/tinystdio/stdio.h:
+/home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/inttypes.h:
+/home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/newlib.h:
+/home/nimunozb/Documentos/embebidos/repositorios/evse-embebidos-software/i5ether/test/EthNativePosix/build/software/include/../libc/picolibc.h:
+/home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/config.h:
+/home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/ieeefp.h:
+/home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/features.h:
+/root/miniconda3/envs/fpga/lib/gcc/riscv32-elf/10.1.0/include/float.h:
+/home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_intsup.h:
+/home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/_ansi.h:
+/home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/stdint.h:
+/home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_default_types.h:
+/home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_stdint.h:
+/root/miniconda3/envs/fpga/lib/gcc/riscv32-elf/10.1.0/include/stddef.h:
+/home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_locale.h:
+/root/miniconda3/envs/fpga/lib/gcc/riscv32-elf/10.1.0/include/stdarg.h:
+/home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_types.h:
+/home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_types.h:
+/home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/string.h:
+/home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/cdefs.h:
+/home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/strings.h:
+/home/nimunozb/Documentos/embebidos/programs/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/string.h:
+/home/nimunozb/Documentos/embebidos/programs/litex/litex/litex/soc/software/libbase/progress.h:
+/home/nimunozb/Documentos/embebidos/programs/litex/litex/litex/soc/software/libliteeth/udp.h:
+/home/nimunozb/Documentos/embebidos/programs/litex/litex/litex/soc/software/libliteeth/tftp.h:
